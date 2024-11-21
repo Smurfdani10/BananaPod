0df8db9f2816e819f929118234b1248aca170ad3
UPN <UPN@ULCLAB1337>
2024-11-21 08:05:08 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace prim2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.rich = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(466, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MATRIZ DE COSTO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(466, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ALGORITMO DE PRIM";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(250, 274);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(464, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // rich
+            // 
+            this.rich.Location = new System.Drawing.Point(249, 102);
+            this.rich.Name = "rich";
+            this.rich.Size = new System.Drawing.Size(466, 138);
+            this.rich.TabIndex = 5;
+            this.rich.Text = "";
+            this.rich.TextChanged += new System.EventHandler(this.rich_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rich);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox rich;
+    }
+}
+
---
+++ Grafo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prim2
+{
+    internal class Grafo
+    {
+        int[,] matriz;
+        int numNodos;
+        public Grafo(int numNodos)
+        {
+            this.numNodos = numNodos;
+            matriz = new int[numNodos, numNodos];
+        }
+
+        public void Insertar() 
+        {
+            matriz[0, 1] = 2;
+            matriz[1,2] = 4;
+            matriz[0,3] = 1;
+            matriz[1,3] = 3;
+            matriz[1,4] = 10;
+            matriz[2,5] = 5;
+            matriz[3,2] = 2;
+            matriz[3,5] = 8;
+            matriz[3,6] = 4;
+            matriz[4,3] = 2;
+            matriz[4,6] = 6;
+            matriz[6,5] = 1;
+        }
+        public void ImprimirMatriz(RichTextBox rich) 
+        {
+            for (int i = 0; i < numNodos; i++) 
+            {
+                for (int j = 0; j < numNodos; j++) 
+                {
+                    rich.Text += "\n";
+                }
+            }
+        }
+    }
+   
+}
